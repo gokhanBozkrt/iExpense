f5606a6eb34d0497dcee142d9ada75bf57fdc8e1
gokhanBozkrt <99559446+gokhanBozkrt@users.noreply.github.com>
2022-04-30 10:26:35 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Gokhan Bozkurt on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
